e4d035c29fa525140226278327da09cc753fac36
Mateus Costa <mateusvoc@gmail.com>
2021-02-01 22:51:45 -0300

    Includes default encryption to s3 buckets

--- scripts/create-s3.sh
+++ scripts/create-s3.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-aws cloudformation create-stack --region us-east-1 --stack-name c3-s3 --template-body file://../c3-s3.yml
+aws cloudformation create-stack --region us-east-1 --stack-name c3-s3 --template-body file://../c3-s3_solution.yml
