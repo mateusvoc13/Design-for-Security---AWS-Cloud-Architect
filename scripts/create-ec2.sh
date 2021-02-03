843526b19e351534f3c72913f83c779ea8c4f436
Mateus Costa <mateusvoc@gmail.com>
2021-02-03 18:31:20 -0300

    Includes tools to the devsecops pipeline better pictures of the security hub and config and policies for the s3 bucket and the instance network

--- scripts/create-ec2.sh
+++ scripts/create-ec2.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-aws cloudformation create-stack --region us-east-1 --stack-name c3-app --template-body file://../c3-app.yml --parameters ParameterKey=KeyPair,ParameterValue="" --capabilities CAPABILITY_IAM
+aws cloudformation create-stack --region us-east-1 --stack-name c3-app --template-body file://../c3-app_solution.yml --parameters ParameterKey=KeyPair,ParameterValue="zenbook-mateus" --capabilities CAPABILITY_IAM
